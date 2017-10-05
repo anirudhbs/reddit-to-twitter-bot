8d29d43e6aeeeeae98ad15a59506a55505eb1ac1
Anirudh B S <anirudhbs96@gmail.com>
2017-10-05 17:26:14 +0530

    Changing directory of config template

---
+++ config/config - template.js
@@ -0,0 +1,14 @@
+const config = {
+  twitterKeys: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  },
+  reddit: {
+    subreddit: '',
+    limit: 
+  }
+}
+
+module.exports = config
