6a0610f7a90cea504a0bed88bea9b1310195f3c6
Anirudh B S <anirudhbs96@gmail.com>
2017-10-05 16:56:04 +0530

    Formatted index js file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const tweet = require('./tweet.js')
-const schedule = require('node-schedule')
+const schedule = require('node-schedule'),
+  tweet = require('./tweet.js')
 
 let rule = new schedule.RecurrenceRule()
-rule.minute = 15
+rule.minute = 0
 console.log('Running...')
-tweet()
-// schedule.scheduleJob(rule, () => {tweet()})
+tweet
+// schedule.scheduleJob(rule, () => {tweet})
