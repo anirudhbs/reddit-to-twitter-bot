da9bbfa4b5dcea5d342b1fb26ecf7d055cf8f173
Anirudh B S <anirudhbs96@gmail.com>
2017-10-05 10:40:09 +0530

    Downloading images without using node-fetch

--- tweet.js
+++ tweet.js
@@ -1,17 +1,16 @@
-const https = require('https')
-const url = 'https://www.reddit.com/r/pics/top/.json?sort=top&t=day&limit=3'
-const async = require('async')
-const fetch = require('node-fetch')
 const fs = require('fs')
+const https = require('https')
+const async = require('async')
+const stream = require('stream')
 
 const getResponse = new Promise(function (resolve, reject) {
+    const url = 'https://www.reddit.com/r/BlackPeopleTwitter/top/.json?sort=top&t=day&limit=2'
     https.get(url, (res) => {
       let data = ''
       res.on('data', (chunk) => {
         data += chunk
       })
       res.on('end', () => {
-        console.log('end')
         temp = JSON.parse(data).data.children
         resolve(temp)
       })
@@ -20,19 +19,25 @@ const getResponse = new Promise(function (resolve, reject) {
   .then(async function (res) {
     const content = res
     await Promise.all(
-      content.map(async function (current) {
+      content.map(function (current) {
         let post = current.data
         let imageUrl = post.preview.images[0].source.url
-        await getImage(imageUrl, post.id)
-        console.log(`${post.title}`)
+        const getImage = new Promise(function (resolve, reject) {
+          https.request(imageUrl, function (response) {
+            let data = new stream.Transform()
+            response.on('data', function (chunk) {
+              data.push(chunk)
+            })
+            response.on('end', function () {
+              fs.writeFileSync(`images/${post.id}.png`, data.read())
+              console.log('done reading')
+            })
+          }).end(function () {
+            console.log(`${post.title}`)
+          })
+        })
       })
     )
   })
 
-const getImage = async function (url, name) {
-  await fetch(url)
-    .then(res => {
-      let file = fs.createWriteStream(`images/${name}.jpg`)
-      res.body.pipe(file)
-    })
-}
+module.exports = getResponse
