044f34c1997cda2d3fd1ebb93aef18ed59fd0356
Anirudh B S <anirudhbs96@gmail.com>
2017-10-10 11:24:17 +0530

    Added local dependencies

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const schedule = require('node-schedule'),
-  tweet = require('./lib/tweet.js')
+  tweet = require('./lib/tweet.js'),
+  config = require('./config/config.js'),  
+  Reddit = config.reddit
 
 let rule = new schedule.RecurrenceRule()
 rule.minute = 0
 console.log('Running...')
-tweet
-// schedule.scheduleJob(rule, () => {tweet})
+// schedule.scheduleJob(rule, () => {
+//   tweet
+// })
+
+const redditUrl = `https://www.reddit.com/r/${Reddit.subreddit}/top/.json?sort=top&t=day&limit=${Reddit.limit}`
+tweet.getContent(redditUrl, tweet.downloadImages)
