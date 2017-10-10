3b0fb71708da64566c9ddfaf9b0aa8affc2278a9
Anirudh B S <anirudhbs96@gmail.com>
2017-10-10 12:20:15 +0530

    Now using rising posts from reddit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const schedule = require('node-schedule'),
   tweet = require('./lib/tweet.js'),
-  config = require('./config/config.js'),  
+  config = require('./config/config.js'),
   Reddit = config.reddit
 
 let rule = new schedule.RecurrenceRule()
 rule.minute = 0
 console.log('Running...')
+const redditUrl = `https://www.reddit.com/r/${Reddit.subreddit}/rising/.json?limit=${Reddit.limit}`
+tweet.getContent(redditUrl, tweet.downloadImages)
 // schedule.scheduleJob(rule, () => {
 //   tweet
 // })
-
-const redditUrl = `https://www.reddit.com/r/${Reddit.subreddit}/top/.json?sort=top&t=day&limit=${Reddit.limit}`
-tweet.getContent(redditUrl, tweet.downloadImages)
