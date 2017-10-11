254e0fc90aa0f9d6a10bcb0ff14fc22b6c488926
Anirudh B S <anirudhbs96@gmail.com>
2017-10-11 14:26:45 +0530

    Updated config template

---
+++ config/config-template.js
@@ -0,0 +1,14 @@
+const config = {
+  twitterKeys: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  },
+  reddit: {
+    subreddit: ['', ''],
+    limit: 10
+  }
+}
+
+module.exports = config
