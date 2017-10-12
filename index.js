ed8b6b1a5f623a6a8e44e7c42be24104b56f4063
Anirudh B S <anirudhbs96@gmail.com>
2017-10-12 11:18:20 +0530

    Added function to create multireddit url from subreddit array

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const schedule = require('node-schedule'),
   tweet = require('./lib/tweet.js'),
-  config = require('./config/config.js'),
-  Reddit = config.reddit
+  config = require('./config/config.js')
 
 let rule = new schedule.RecurrenceRule()
 rule.minute = 31
-let subreddits = Reddit.subreddit.join('+'),
-  redditUrl = `https://www.reddit.com/r/${subreddits}/rising/.json?limit=${Reddit.limit}`
 
 console.log('Running...')
-tweet.getContent(redditUrl, tweet.filterContent)
+tweet.run()
+
 // schedule.scheduleJob(rule, () => {
-//   tweet.getContent(redditUrl, tweet.filterContent)
+//  tweet.run()
 // })
