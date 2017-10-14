0c6c2b0b1ae3c31e27a5eaa61f1eb6188db6dc75
Anirudh B S <anirudhbs96@gmail.com>
2017-10-15 02:38:33 +0530

    reduced no. of tweets

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const tweet = require('./lib/tweet.js')
 
 console.log('Running...')
 tweet.run()
-// setInterval(function () {
-//   tweet.run()
-// }, 20 * 60 * 1000) //20 min interval
+setInterval(function () {
+  tweet.run()
+}, 20 * 60 * 1000) //20 min interval
